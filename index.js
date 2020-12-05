5e017a5ef56332c1d63d57e052988b67180b0579
Omar Rizwan <omar@omar.website>
2020-12-05 02:33:26 -0800

    stop keyDown propagation, make editor for each details entry

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 require.config({ paths: { 'vs': __vsPath }});
 require(['vs/editor/editor.main'], function() {
-  var editor = monaco.editor.create(document.getElementsByClassName('details')[0], {
-    value: [
-      'function x() {',
-      '\tconsole.log("Hello world!");',
-      '}'
-    ].join('\n'),
-    language: 'javascript'
-  });
-  editor.onKeyDown(function(e) {
+  function makeEditor(details) {
+    var editor = monaco.editor.create(details, {
+      value: [
+        'function x() {',
+        '\tconsole.log("Hello world!");',
+        '}'
+      ].join('\n'),
+      language: 'javascript'
+    });
+    details.addEventListener('keydown', e => {
+      e.stopPropagation();
+    });
+  }
 
-  });
+  [...document.getElementsByClassName('details')].forEach(makeEditor);
 });
