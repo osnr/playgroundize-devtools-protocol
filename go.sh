f90913d816b459b19614a3c4e501f8dc5653c4a9
Omar Rizwan <omar@omar.website>
2020-12-24 16:18:01 -0800

    script to reload

---
+++ go.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eux
+echo false > /Users/osnr/Code/tabfs/fs/mnt/extensions/Playg*/enabled
+echo true > /Users/osnr/Code/tabfs/fs/mnt/extensions/Playg*/enabled
+echo reload > /Users/osnr/Code/tabfs/fs/mnt/tabs/by-title/Chrome_Dev*/control
